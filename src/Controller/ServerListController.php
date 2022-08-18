a95a3436fc51d3d4d6e44e64ae56c6371835dd0a
Tamas Soos <tamas.soos@sap.com>
2022-08-18 22:39:51 +0200

    Added server list upload form

--- src/Controller/ServerListController.php
+++ src/Controller/ServerListController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\ServerList;
+use App\Form\ServerListType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ServerListController extends AbstractController
 {
     /**
-     * @Route("/server/list", name="app_server_list")
+     * @Route("/server/list/upload", name="app_server_list_upload")
      */
-    public function index(): Response
+    public function upload(Request $request, SluggerInterface $slugger): Response
     {
-        return $this->render('server_list/index.html.twig', [
-            'controller_name' => 'ServerListController',
+        //TODO: handle deletion of old file
+
+        $form = $this->createForm(ServerListType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $serverListFile = $form->get('fileName')->getData();
+
+            $originalFilename = pathinfo($serverListFile->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$serverListFile->guessExtension();
+
+            // Move the file to the directory where brochures are stored
+            try {
+                $serverListFile->move(
+                    $this->getParameter('server_list_directory'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                //TODO: handle exception if something happens during file upload
+            }
+
+            ServerList::setFileName($newFilename);
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->renderForm('server_list/upload.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- src/Form/ServerListType.php
+++ src/Form/ServerListType.php
@@ -4,15 +4,34 @@ namespace App\Form;
 
 use App\Entity\ServerList;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ServerListType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('fileName')
+            ->add('fileName', FileType::class, [
+                'label' => 'Server List (Excel file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                'required' => true,
+
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/vnd.ms-excel',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid MS Excel sheet',
+                    ])
+                ],
+            ])
         ;
     }
 
