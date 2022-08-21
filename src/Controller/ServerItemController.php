fb9c93ec5d77a6e84aaf765d5c4cd5bfe4cd9ab5
Tamas Soos <tamas.soos@sap.com>
2022-08-21 17:53:24 +0200

    Added ID as the first column to server table

--- src/Controller/ServerItemController.php
+++ src/Controller/ServerItemController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\ServerItem;
+use App\Repository\ServerItemRepository;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +14,10 @@ class ServerItemController extends AbstractServerItemController
     /**
      * @Route("/server/item/list", name="app_server_item_list")
      */
-    public function list(): Response
+    public function list(ManagerRegistry $doctrine): Response
     {
-        return $this->render('server_item/list.html.twig', []);
+        return $this->render('server_item/list.html.twig', [
+            'server_items' => $doctrine->getRepository(ServerItem::class)->findAll()
+        ]);
     }
 }
