308a3105903db2fe7b2d82f7dcdfbfefc46386d8
Tamas Soos <tamas.soos@sap.com>
2022-08-24 18:07:51 +0200

    Added slider with default config to filtering

--- assets/controllers/filter_controller.js
+++ assets/controllers/filter_controller.js
@@ -1,6 +1,20 @@
 import { Controller } from '@hotwired/stimulus';
+import noUiSlider from 'nouislider';
+
 
 export default class extends Controller {
+    connect() {
+        super.connect();
+        let sliderStorageCapacity = document.getElementById('formFilterSliderStorageCapacity');
+        noUiSlider.create(sliderStorageCapacity, {
+            start: 1,
+            range: {
+                'min': 0,
+                'max': 8
+            }
+        });
+    }
+
     expand(event) {
         let filter_section = document.getElementById("collapseFilter");
         filter_section.classList.contains('show') ? filter_section.classList.remove('show') : filter_section.classList.add('show');
