15d9030525f6ab436b975449c5309a3c01908673
Tamas Soos <tamas.soos@sap.com>
2022-08-24 13:32:51 +0200

    Bug fix

--- assets/controllers/filter_controller.js
+++ assets/controllers/filter_controller.js
@@ -5,9 +5,9 @@ export default class extends Controller {
     //     this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
     // }
     expand(event) {
-        event.preventDefault();
-        console.log('Expand called');
-        let filter_section = $('.collapse');
+        // event.preventDefault();
+        // console.log('Expand called');
+        let filter_section = document.getElementById("collapseFilter");
         filter_section.classList.contains('show') ? filter_section.classList.remove('show') : filter_section.classList.add('show');
     }
 }
