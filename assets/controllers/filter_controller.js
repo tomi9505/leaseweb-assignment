66609c5c6a03a7760129ef0b6d6cc43cbfe6780c
Tamas Soos <tamas.soos@sap.com>
2022-08-24 13:39:20 +0200

    First iteration of form for filtering

--- assets/controllers/filter_controller.js
+++ assets/controllers/filter_controller.js
@@ -1,12 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    // connect() {
-    //     this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
-    // }
     expand(event) {
-        // event.preventDefault();
-        // console.log('Expand called');
         let filter_section = document.getElementById("collapseFilter");
         filter_section.classList.contains('show') ? filter_section.classList.remove('show') : filter_section.classList.add('show');
     }
