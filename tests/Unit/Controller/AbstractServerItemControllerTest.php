be3b6c46ced05084e84c017a28c42b2eef1a048a
Tamas Soos <tomi9505@gmail.com>
2022-08-27 17:29:01 +0200

    Initial commit of AbstractServerItemControllerTest

---
+++ tests/Unit/Controller/AbstractServerItemControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class AbstractServerItemControllerTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
