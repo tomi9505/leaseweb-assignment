b1f4a793bf8a19cdece24a22c2b16a315a82ef54
Tamas Soos <tamas.soos@sap.com>
2022-08-27 18:15:01 +0200

    Bug fix

--- tests/Unit/Controller/AbstractServerItemControllerTest.php
+++ tests/Unit/Controller/AbstractServerItemControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Unit\Controller;
 
 use App\Controller\AbstractServerItemController;
+use App\Repository\ServerItemRepository;
 use App\Tests\TestUtil;
 use Doctrine\Persistence\ManagerRegistry;
 use PHPUnit\Framework\TestCase;
@@ -10,25 +11,30 @@ use PHPUnit\Framework\TestCase;
 class AbstractServerItemControllerTest extends TestCase
 {
     private $doctrine;
+    private $repository;
     private $controller;
 
     public function setUp(): void
     {
         parent::setUp();
+        $this->repository = $this->createMock(ServerItemRepository::class);
         $this->doctrine = $this->createMock(ManagerRegistry::class);
+        $this->repository->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($this->repository);
         $this->controller = new AbstractServerItemController();
     }
 
     public function testGetAllServerItemsEmpty() {
         // Given: there are no server items in the database
-        $this->doctrine
+        $this->repository
             ->expects($this->any())
             ->method('findAll')
             ->willReturn([]);
 
         // When: I query all server items
         try {
-            $result = TestUtil::callMethod($this->controller, 'getAllServerItems', []);
+            $result = TestUtil::callMethod($this->controller, 'getAllServerItems', [$this->doctrine]);
         } catch (\ReflectionException $e) {
             $this->fail('Failed to call method \'getAllServerItems\' under test');
         }
