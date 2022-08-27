460a9051499888eeadb01f85652768f1398fe6b7
Tamas Soos <tamas.soos@sap.com>
2022-08-27 17:56:38 +0200

    Added first unit test for AbstractServerItemController

--- tests/Unit/Controller/AbstractServerItemControllerTest.php
+++ tests/Unit/Controller/AbstractServerItemControllerTest.php
@@ -2,12 +2,38 @@
 
 namespace App\Tests\Unit\Controller;
 
+use App\Controller\AbstractServerItemController;
+use App\Tests\TestUtil;
+use Doctrine\Persistence\ManagerRegistry;
 use PHPUnit\Framework\TestCase;
 
 class AbstractServerItemControllerTest extends TestCase
 {
-    public function testSomething(): void
+    private $doctrine;
+    private $controller;
+
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->doctrine = $this->createMock(ManagerRegistry::class);
+        $this->controller = new AbstractServerItemController();
+    }
+
+    public function getAllServerItemsEmpty() {
+        // Given: there are no server items in the database
+        $this->doctrine
+            ->expects($this->any())
+            ->method('findAll')
+            ->willReturn([]);
+
+        // When: I query all server items
+        try {
+            $result = TestUtil::callMethod($this->controller, 'getAllServerItems', []);
+        } catch (\ReflectionException $e) {
+            $this->fail('Failed to call method \'getAllServerItems\' under test');
+        }
+
+        // Then: the response is an empty array
+        $this->assertCount(0, $result);
     }
 }
