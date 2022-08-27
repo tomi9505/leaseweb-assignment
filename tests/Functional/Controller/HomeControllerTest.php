01b091fb8f8e1da3806f88c6e405f6660a5dcdb0
Tamas Soos <tomi9505@gmail.com>
2022-08-27 18:49:12 +0200

    Initial commit of HomeControllerTest

---
+++ tests/Functional/Controller/HomeControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Functional\Controller;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class HomeControllerTest extends PantherTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+}
