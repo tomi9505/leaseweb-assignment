f886e84c7326608773c278722df6e4918ef40af2
Tamas Soos <tomi9505@gmail.com>
2022-08-27 17:21:14 +0200

    Initial commit of ServerItemRepositoryTest

---
+++ tests/Unit/Repository/ServerItemRepositoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit\Repository;
+
+use PHPUnit\Framework\TestCase;
+
+class ServerItemRepositoryTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
